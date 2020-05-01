0aaeb18f21579fc31c340a7e2c7e3e1012357013
KESHAV GUPTA <keshav.gupta@iiitb.org>
2020-05-01 15:41:26 +0530

    Update CentralSellerPortalApplication.java

--- src/main/java/com/acms/CentralSellerPortal/CentralSellerPortalApplication.java
+++ src/main/java/com/acms/CentralSellerPortal/CentralSellerPortalApplication.java
@@ -3,6 +3,7 @@ package com.acms.CentralSellerPortal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 
 @SpringBootApplication
 @EnableJpaRepositories
